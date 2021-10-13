ee6cd5455adc8e9d6edbb1a249e62e589e586f0b
Alisher <89141153+Katak1@users.noreply.github.com>
2021-10-13 12:18:17 +0600

    Update serializers.py
    Fixed account

--- account/serializers.py
+++ account/serializers.py
@@ -10,26 +10,35 @@ User = get_user_model()
 class RegistrationSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(min_length=4, required=True)
-    password_confirmation = serializers.CharField(min_length=4, required=True)
+    password_confirm = serializers.CharField(
+        min_length=4, required=True
+    )
     name = serializers.CharField(required=True)
     last_name = serializers.CharField()
 
     def validate_email(self, email):
         if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError('Пользователь с таким email уже зарегистрирован')
+            raise serializers.ValidationError(
+                "Пользователь с таким email уже зарегистрирован"
+            )
         return email
 
     def validate(self, data):
         password = data.get('password')
-        password_confirmation = data.pop('password_confirmation')
-        if password_confirmation != password:
-            raise serializers.ValidationError('Пароли не совпадают')
+        password_confirm = data.pop('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError(
+                "Пароль не совпадает"
+            )
+        print(data)
         return data
 
     def create(self, validated_data):
-        user = User.objects.create(**validated_data)
+        user = User.objects.create_user(**validated_data)
         user.create_activation_code()
-        send_activation_code(user.email, user.activation_code)
+        send_activation_code(
+            user.email, user.activation_code
+        )
         return user
 
 
